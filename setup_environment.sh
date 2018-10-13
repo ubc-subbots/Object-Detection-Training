34b4e4c813aadead635893168e109f8b38f04454
Cameron Newton <newtoncameron@hotmail.com>
2018-10-13 13:11:47 -0700

    Changed structure

---
+++ setup_environment.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# The current directory
+CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "================================================================"
+echo "Setup .bashrc"
+echo "================================================================"
+# Shell config files that various shells source when they run.
+# This is where we want to add aliases, source ROS environment
+# variables, etc.
+SHELL_CONFIG_FILES=(
+    "$HOME/.bashrc"\
+            "$HOME/.zshrc"
+    )
+
+# All lines listed here will be added to the shell config files
+# listed above, if they are not present already
+declare -a new_shell_config_lines=(
+    # Make sure that all shells know where to find our custom gazebo models,
+    # plugins, and resources. Make sure to preserve the path that already exists as well
+    "export PYTHONPATH=$PYTHONPATH:$CURR_DIR/models/research:$CURR_DIR/models/research/slim"
+)
+
+# Add all of our new shell config options to all the shell
+# config files, but only if they don't already have them
+for file_name in "${SHELL_CONFIG_FILES[@]}"; 
+do
+    echo "Setting up $file_name"
+    for line in "${new_shell_config_lines[@]}"; 
+    do
+        if ! grep -Fq "$line" $file_name 
+        then
+            echo "$line" >> $file_name 
+        fi
+    done
+done
+
+#Installing python dependencies
+pip install virtualenv
+
+virtualenv subbots_python
+source $CURR_DIR/subbots_python/bin/activate
+pip install -r $CURR_DIR/requirements.txt
+
+wget -O models/research/protobuf.zip https://github.com/google/protobuf/releases/download/v3.0.0/protoc-3.0.0-linux-x86_64.zip
+unzip models/research/protobuf.zip -d models/research
+
+./bin/protoc models/research/object_detection/protos/*.proto --python_out=/models/research
+
+export PYTHONPATH=$PYTHONPATH:$CURR_DIR/models/research:$CURR_DIR/models/research/slim
+
+
+
+#Testing installation
+python $CURR_DIR/research/object_detection/builders/model_builder_test.py
+
+
+
