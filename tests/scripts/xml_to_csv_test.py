1658b272f613f602daa88ffdb50cad34b683c392
Cameron Newton <newtoncameron@hotmail.com>
2019-01-14 21:03:57 -0800

    Fixed test crash

--- tests/scripts/xml_to_csv_test.py
+++ tests/scripts/xml_to_csv_test.py
@@ -5,7 +5,7 @@
 import os
 import pytest
 
-def xml_to_csv_test():
+def test_xml_to_csv():
 	# Run xml_to_csv script
 	os.chdir(os.path.dirname(os.path.realpath(__file__)))
 	
@@ -16,6 +16,8 @@ def xml_to_csv_test():
 
 	assert 'filename,width,height,class,xmin,ymin,xmax,ymax' in result
 	assert 'gate_0000.jpg,1920,1080,Gate,1363,298,1916,742' in result
+	
 
 if __name__ == "__main__":
 	xml_to_csv_test()
+	pass
