6121842846099f6c5fb766cd037806f58ef400f0
newtoncn <newtoncameron@hotmail.com>
2019-02-16 11:28:49 -0800

    Updated model type

--- data/models/download_model.sh
+++ data/models/download_model.sh
@@ -3,4 +3,4 @@
 # The current directory
 CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-wget http://download.tensorflow.org/models/object_detection/faster_rcnn_resnet101_coco_2018_01_28.tar.gz -P $CURR_DIR
+wget http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_coco_2018_01_28.tar.gz -P $CURR_DIR
